71222dd80c7e938d37f01bc305f177edfdb6eee0
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-08-15 18:07:25 +0900

    search_fix

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,10 +3,13 @@ class Result < ApplicationRecord
   belongs_to :work
 
   def self.search(search)
-    if search[:month].to_i >= 10
-      Result.where(user_id: "#{search[:user_id]}").where('created_at LIKE(?)', "#{search[:year]}-#{search[:month]}-%" )
-    else
-      Result.where(user_id: "#{search[:user_id]}").where('created_at LIKE(?)', "#{search[:year]}-0#{search[:month]}-%" )
-    end
+    search_time = "#{search[:year]}-#{search[:month]}-01"
+    Result.where(user_id: "#{search[:user_id]}").where(created_at: search_time.in_time_zone.all_month)
+    
+    #if search[:month].to_i >= 10
+    #  Result.where(user_id: "#{search[:user_id]}").where('created_at: LIKE(?)', "#{search[:year].to_i}-#{search[:month].to_i}-%" )
+    #else
+    #  Result.where(user_id: "#{search[:user_id]}").where('created_at: LIKE(?)', "#{search[:year].to_i}-0#{search[:month].to_i}-%" )
+    #end
   end
 end
